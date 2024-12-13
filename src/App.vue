63fd47d721c079e6a3c0279efe9863ccd39522e1
raghnat <rayhan.aghnat@mhs.unsoed.ac.id>
2024-12-13 19:36:49 +0700

    el done

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,15 @@
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from "@ionic/vue";
 import Bottombar from "@/components/Bottombar.vue";
-import { computed, onMounted, watch } from "vue";
+import { computed, onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 
 import { useAuthStore } from "./authStore";
 import router from "./router";
+import ManageUserPage from "./views/admin/manage/users/ManageUserPage.vue";
 
 const route = useRoute();
+const user = ref("");
 
 // Menentukan halaman mana yang harus menampilkan Bottombar
 const showTabs = computed(() => {
@@ -25,25 +27,37 @@ const showTabs = computed(() => {
 // Akses authStore
 const authStore = useAuthStore();
 
+//rute
+const guestRoute = ["Login", "Register"];
+const adminRoute = ["ManageUser", "ManageTarget", "ManageCategory"];
+
 // Pantau perubahan auth dan navigasi
 onMounted(() => {
   authStore.loadUserFromLocalStorage(); // Ambil data dari localStorage
+  user.value = authStore.currentUser;
+  console.log("Info user", user.value);
 
   // // Cek jika pengguna belum login, alihkan ke login
   // if (!authStore.isAuthenticated()) {
   //   if (["Login", "Register"].includes(route.name as string)) {
   //     return; // Biarkan pengguna tetap di halaman login/register
   //   }
+  //   alert("Anda melakukan hal ilegal");
   //   router.replace("/login"); // Arahkan ke login jika belum login
   // }
 });
 
-// watch(
-//   () => authStore.currentUser,
-//   (user) => {
-//     if (user && ["Login", "Register"].includes(route.name as string)) {
-//       router.replace("/home"); // Arahkan ke Home jika sudah login
-//     }
-//   }
-// );
+watch([() => authStore.currentUser, () => route.name], ([x, y]) => {
+  // if (x && guestRoute.includes(y as string)) {
+  //   router.replace("/home"); // Arahkan ke Home jika sudah login
+  // }
+  // // Jika pengguna bukan admin dan mencoba akses halaman admin
+  // if (
+  //   adminRoute.includes(y as string) &&
+  //   (!x || (x.role !== "Administrator" && x.role !== "Admin"))
+  // ) {
+  //   router.replace("/home");
+  //   alert("Anda tidak memiliki izin untuk mengakses halaman ini.");
+  // }
+});
 </script>
